696cd63627358b50bfae947a3160ffa7f5ae49d2
wrcnpst <wichai.rcnpst@gmail.com>
2023-10-22 14:01:26 +0700

    Created Banklist webapp functions
    only timer left

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -47,12 +47,14 @@ const labelTimer = document.querySelector('.timer');
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
 
+// buttons
 const btnLogin = document.querySelector('.login__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
 
+// inputs
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
@@ -74,3 +76,182 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
+const arr = [23, 11, 64];
+
+// getting array the last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr[-1]); // <-- undefined because array in js is zero based
+*/
+
+// pre-process
+const createUsernames = () => {
+  accounts.forEach(account => {
+    const ownerArr = account.owner.toLowerCase().split(' ');
+    let user = '';
+    ownerArr.forEach(str => {
+      user += str[0];
+    });
+
+    account.userName = user;
+  });
+};
+
+createUsernames();
+
+inputLoginUsername.value = 'js';
+inputLoginPin.value = '1111';
+
+// FUNCTIONS
+
+// refresh data
+function refresh(acc = currentAccount) {
+  reCalculate(acc);
+  reMovement(acc);
+}
+
+// calculation
+function reCalculate(acc) {
+  let balance = 0;
+  let intrans = 0;
+  let outtrans = 0;
+  let interest = 0;
+
+  acc.movements.forEach(trans => {
+    balance += trans;
+
+    if (trans > 0) intrans += trans;
+    else if (trans < 0) outtrans += trans;
+  });
+
+  // calc interest
+  interest = (balance * acc.interestRate) / 100;
+
+  // lable DOM
+  labelBalance.textContent = balance + '€';
+  labelSumIn.textContent = intrans + '€';
+  labelSumOut.textContent = -outtrans + '€';
+  labelSumInterest.textContent = interest + '€';
+}
+
+// movement board
+function reMovement(acc) {
+  containerMovements.innerHTML = '';
+
+  acc.movements.forEach((amount, i) => {
+    const type = amount < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${amount}€</div>
+  </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+// EVENTLISTENER
+let currentAccount = null;
+
+// login button
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAccount = accounts.find(acc => {
+    return inputLoginUsername.value === acc.userName;
+  });
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    console.log('login successfully');
+    inputLoginUsername.value = null;
+    inputLoginPin.value = null;
+
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.slice(
+      0,
+      currentAccount.owner.indexOf(' ')
+    )}`;
+
+    containerApp.style.opacity = 100;
+    refresh();
+  } else console.log('email or password wong');
+});
+
+// Request loan
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const loan = Number(inputLoanAmount.value);
+
+  if (0 < loan) {
+    currentAccount.movements.push(loan);
+    refresh();
+    inputLoanAmount.value = null;
+  }
+});
+
+// Transfer money
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const to = inputTransferTo.value;
+  const amount = Number(inputTransferAmount.value);
+  const balance = Number(labelBalance.textContent.slice(0, -1));
+
+  const hasDestinationAcc = accounts.find(account => {
+    return to !== currentAccount.userName && to === account.userName;
+  });
+
+  console.log(to, hasDestinationAcc, amount, balance);
+
+  if (hasDestinationAcc && 0 < amount <= balance) {
+    console.log('transfering');
+    currentAccount.movements.push(-amount);
+    hasDestinationAcc.movements.push(amount);
+    refresh();
+    inputTransferTo.value = null;
+    inputTransferAmount.value = null;
+  }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(currentAccount);
+  if (
+    inputCloseUsername.value === currentAccount.userName &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    console.log('deleting');
+
+    const indexToRemove = accounts.findIndex(acc => {
+      return acc.userName === currentAccount.userName;
+    });
+
+    accounts.splice(indexToRemove, 1);
+
+    currentAccount = null;
+    inputCloseUsername.value = null;
+    inputClosePin.value = null;
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = 'Log in to get started';
+  }
+});
+
+let sortDirection = 'asc';
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (sortDirection === 'asc') {
+    console.log('to asc');
+    currentAccount.movements.sort((a, b) => a - b);
+    sortDirection = 'desc';
+  } else {
+    console.log('to desc');
+    currentAccount.movements.sort((a, b) => b - a);
+    sortDirection = 'asc';
+  }
+
+  refresh();
+});
